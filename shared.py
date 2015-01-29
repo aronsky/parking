022be4cd3017f28f8f0995939060513fad579c2b
aronsky <aronsky@gmail.com>
2015-01-29 18:10:51 +0200

    Added downtime checking function

--- shared.py
+++ shared.py
@@ -1,3 +1,8 @@
+import datetime
+import time
+import hashlib
+import random
+
 #############
 # Constants #
 #############
@@ -5,6 +10,20 @@
 INSIDE_SPOTS_COUNT = 5
 OUTSIDE_SPOTS_COUNT = 11
 GUEST_PLATE = 9999999
+
+############
+# Timezone #
+############
+
+class Ist(datetime.tzinfo):
+    def dst(self, dt):
+        return datetime.timedelta(0)
+
+    def tzname(self, dt):
+        return "IST-02IDT"
+
+    def utcoffset(self, dt):
+        return datetime.timedelta(hours=2)
 
 
 ##############
@@ -39,3 +58,10 @@ class SpotNotReservedException(ParkingException):
 def make_name(user):
     return user.nickname().split('@')[0].replace('.', ' ').title()
     
+def is_downtime():
+    current_time = datetime.datetime.now(Ist())
+    minimum_time = current_time.replace(hour=5, minute=45, second=0, microsecond=0)
+    seed_base = current_time.strftime("Year: %Y Month: %m Day:%d")
+    seed = int(hashlib.new("md5", seed_base).hexdigest(), 16) % 1024
+    randomizer = random.Random(seed)
+    return not (current_time - minimum_time).total_seconds() > 60 * randomizer.randint(0,60)
